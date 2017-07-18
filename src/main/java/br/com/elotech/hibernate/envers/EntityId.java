76c391f95b4adf81d0da11fcb0ffe8892f45c4d6
Rodolfo Jordão <rodolforjb@gmail.com>
2017-07-18 17:23:53 -0300

    Removed extends serializable as it is unnecessary

--- src/main/java/br/com/elotech/hibernate/envers/EntityId.java
+++ src/main/java/br/com/elotech/hibernate/envers/EntityId.java
@@ -1,8 +1,6 @@
 package br.com.elotech.hibernate.envers;
 
-import java.io.Serializable;
-
-public interface EntityId<ID extends Serializable> {
+public interface EntityId<ID> {
 
     ID getId();
 
